45de539b13c2820b9f023d4acceea27d52ceb8f8
tonnique <tonique@bk.ru>
2018-12-16 23:32:54 +0300

    Here I added a simple example of WebFilter. This filter runs before SimpleServlet servlet is invoked and puts messages into server logs

---
+++ src/main/java/ru/antony/SimpleFilter.java
@@ -0,0 +1,47 @@
+package ru.antony;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebFilter(
+        filterName = "SimpleFilter",
+        initParams = {
+           @WebInitParam(name="filterparam1", value="filtervalue1")
+        },
+        urlPatterns = {"/simpleservlet"}
+    )
+public class SimpleFilter implements Filter {
+    private FilterConfig filterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        ServletContext servletContext = filterConfig.getServletContext();
+        servletContext.log("Входим в doFilter()");
+        servletContext.log("... инициализируем параметры: ");
+        Enumeration<String> initParameterNames = filterConfig.getInitParameterNames();
+        String parameterName, parameterValue;
+
+        while (initParameterNames.hasMoreElements()) {
+            parameterName = initParameterNames.nextElement();
+            parameterValue = filterConfig.getInitParameter(parameterName);
+            servletContext.log(parameterName + " = " + parameterValue);
+        }
+        servletContext.log("Вызываем сервлет ...");
+        filterChain.doFilter(servletRequest, servletResponse);
+        servletContext.log("Возвращаемся из сервлета");
+    }
+
+    @Override
+    public void destroy() {
+        filterConfig = null;
+    }
+}
